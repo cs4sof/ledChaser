42b9ca5e36fed1eb77340f7f25fac17d546872b0
hungryTechie <13680633+irimina@users.noreply.github.com>
2020-09-18 20:05:02 -0400

    Update reWriteMe.ino

--- reWriteMe.ino
+++ reWriteMe.ino
@@ -2,10 +2,11 @@
 
 // LED Base Chaser Code
 
-//Task #1: Describe the components most likely used in this circuit. 
+//Task #1: Based on the code below, describe the components most likely needed for in this circuit. 
 //Task #2: Rewrite the following code in any way you want as long as you: (1) reduce the lines of code, (2) improve readability and maintenance and (3) has no errors
-//Task #3: Include comments in your code and describe (1) what the initial code does, (2) what your modified code does and (3) why is you code better than the initial code.
+//Task #3: Include comments in your code and describe (1) what your modified code does and (2) why is you code better than the initial code.
 
+// NOTE: You cam modify the timing/delay of how LEDs light on or off.  
 
 int ledPin01 =  13;  
 int ledPin02 = 12;   
